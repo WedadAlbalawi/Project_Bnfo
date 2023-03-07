76713ff9f934425edc0059841965a81be2331a48
Wedad Albalawi <51763094+WedadAlbalawi@users.noreply.github.com>
2023-03-07 13:22:31 +0300

    Update Micro_VariantsCalling_bcftools.sh

--- project_vcf/Micro_VariantsCalling_bcftools.sh
+++ project_vcf/Micro_VariantsCalling_bcftools.sh
@@ -17,8 +17,8 @@ bcftools
 '''
 
 #Step 1 : create conda enviromant 
-cd /Users/wedadalbalawi
-# remove the channels of it's exists 
+cd /Users/wedadalbalawi/project_vcf/
+# remove the channels of its exists 
 conda config --remove channels bioconda
 conda config --remove channels conda-forge
 #recreate
@@ -31,6 +31,10 @@ conda config --show channels
 #-----------------------------------------------#
 #Step2: Create the env variable for tools  that use 
 conda create -n variants bwa bcftools samtools sickle python biopython
+
+#To install this package run one of the following:
+conda install -c bioconda libdeflate
+conda install -c "bioconda/label/cf201901" libdeflate
 
 # Activate conda variants by run 
 conda activate variants
@@ -55,36 +59,37 @@ sickle pe -f P7741_R1.fastq.gz -r P7741_R2.fastq.gz -t sanger -q 20 -l 20 -g -o 
 echo "Triming finished running!"
 #---------------------------------------------------------#
 #Step Two alignment with ref genome Agy99
-'''
+
 #Step Two alignment with ref genome Agy99
-#bwa mem -t 8 ~/project_vcf/ref/Agy99.fasta ~/project_vcf/trimmed_R1.fastq.gz ~/project_vcf/trimmed_R2.fastq.gz > ~/project_vcf/output.sam
+bwa mem -t 8 ~/project_vcf/ref/Agy99.fasta ~/project_vcf/trimmed_R1.fastq.gz ~/project_vcf/trimmed_R2.fastq.gz > ~/project_vcf/output.sam
 
 #Step third confired sam to baw to minamized the size
-#samtools view -S -b output.sam > ~/project_vcf/output.bam
+samtools view -S -b output.sam > ~/project_vcf/output.bam
 
 #Check the size of sam and bam file by run 
     #du -sh output.sam 
     #du -sh output.bam 
-'''
+
 
 # with this the index file will be in the same dir of your reference
 
-bwa index /Users/wedadalbalawi/Desktop/project_vcf/ref/Agy99.fasta
+bwa index /Users/wedadalbalawi/project_vcf/ref/Agy99.fasta
 
 #bwa mem -t 8 /Users/wedadalbalawi/project_vcf/ref/Agy99.fasta /Users/wedadalbalawi/project_vcf/data/trimmed_R1.fastq.gz /Users/wedadalbalawi/project_vcf/data/trimmed_R2.fastq.gz > /Users/wedadalbalawi/project_vcf/data/output.sam
  #- for the output from first command
-bwa mem -t 8 ~/Desktop/project_vcf/ref/Agy99.fasta ~/Desktop/project_vcf/data/trimmed_R1.fastq.gz ~/Desktop/project_vcf/data/trimmed_R2.fastq.gz | samtools sort -o ~/Desktop/project_vcf/data/output.sorted.bam -
+bwa mem -t 8 ~/project_vcf/ref/Agy99.fasta ~/project_vcf/data/trimmed_R1.fastq.gz ~/project_vcf/data/trimmed_R2.fastq.gz | samtools sort -o ~/project_vcf/data/output.sorted.bam -
 echo "Alignment and sorted finished running!"
 
+
 #Step Fifth bcftools mpileup 
-#bcftools mpileup -O b -o raw.bcf -f ~/project_vcf/ref/Agy99.fasta --threads 8 -q 20 -Q 30 ~/project_vcf/data/output.sorted.bam 
+bcftools mpileup -O b -o ~/project_vcf/data/raw.bcf -f ~/project_vcf/ref/Agy99.fasta --threads 8 -q 20 -Q 30 ~/project_vcf/data/output.sorted.bam 
 echo " bcftools mpileup running!"
-bcftools call --ploidy 1 -m -v -o ~/Desktop/project_vcf/data/variants.raw.vcf ~/Desktop/project_vcf/data/raw.bcf
+bcftools call --ploidy 1 -m -v -o ~/project_vcf/data/variants.raw.vcf ~/project_vcf/data/raw.bcf
 
 #Expplation 
 #count the variants
 #grep -v -c '^#' variants.raw.vcf 
 # count snp  & indels varants
-bcftools view -v snps ~/Desktop/project_vcf/data/variants.raw.vcf | grep -v -c '^#' 
-bcftools view -v indels  ~/Desktop/project_vcf/data/variants.raw.vcf | grep -v -c '^#' 
-bcftools query -f '%POS\n' ~/Desktop/project_vcf/data/variants.raw.vcf >~/Desktop/project_vcf/data/pos.txt
+bcftools view -v snps ~/project_vcf/data/variants.raw.vcf | grep -v -c '^#' 
+bcftools view -v indels  ~/project_vcf/data/variants.raw.vcf | grep -v -c '^#' 
+bcftools query -f '%POS\n' ~/project_vcf/data/variants.raw.vcf >~/project_vcf/data/pos.txt
